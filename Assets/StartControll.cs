00ead35c963ea98d3d635360d101a979b2ce5e15
dlstjd0237 <a49918074@gmail.com>
2023-09-01 00:51:55 +0900

    TitleScenes

--- Assets/StartControll.cs
+++ Assets/StartControll.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
+using TMPro;
 public class StartControll : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<TextMeshPro> Text = new List<TextMeshPro>();
+    [SerializeField] private TextMeshProUGUI _toych;
+    private Color _cr;
+    public void OnText()
     {
-        
+        for (int i = 0; i < Text.Count; i++)
+        {
+            Text[i].fontMaterial.DOFloat(0, ShaderUtilities.ID_FaceDilate, 3);
+        }
+        StartCoroutine(TextFadeOut());
     }
-
-    // Update is called once per frame
-    void Update()
+    private IEnumerator TextFadeOut()
     {
-        
+        _cr = _toych.color;
+        while (_toych.color.a <= 1)
+        {
+            _cr.a += Time.deltaTime / 2f;
+            _toych.color = _cr;
+            yield return null;
+        }
+        StartCoroutine(TextFadeIn());
+    }
+    private IEnumerator TextFadeIn()
+    {
+        _cr = _toych.color;
+        while (_toych.color.a >= 0)
+        {
+            _cr.a -= Time.deltaTime / 2f;
+            _toych.color = _cr;
+            yield return null;
+        }
+        StartCoroutine(TextFadeOut());
     }
 }
